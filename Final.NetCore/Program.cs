c6fe21e535bdd1ba7e30076ae45fac04224554c5
Sled <ibrahimmirheshimov@gmail.com>
2026-01-21 02:35:35 +0400

    File operations and mini test

--- Final.NetCore/Program.cs
+++ Final.NetCore/Program.cs
@@ -1,6 +1,10 @@
-﻿namespace Final.NetCore
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Final.NetCore
 {
-    class Worker
+    public class Worker
     {
         public Guid Id { get; set; }
 
@@ -83,8 +87,25 @@
                 _age = value;
             }
         }
+
+        public CV? cV { get; set; }
+        public Worker(string? name, string? surname, string? residencyCity, string? phoneNo, int age, CV cv)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            ResidencyCity = residencyCity;
+            PhoneNo = phoneNo;
+            Age = age;
+            cV = cv;
+        }
+
+        public Worker()
+        {
+
+        }
     }
-    class Employer
+    public class Employer
     {
         public Guid Id { get; set; }
         private string? _name;
@@ -163,11 +184,25 @@
                 _age = value;
             }
         }
-
         public List<string>? Vacancies { get; set; }
+
+        public Employer()
+        {
+
+        }
+        public Employer(string? name, string? surname, string? residencyCity, string? phoneNo, int age, List<string>? vacancies)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            ResidencyCity = residencyCity;
+            PhoneNo = phoneNo;
+            Age = age;
+            Vacancies = vacancies;
+        }
     }
 
-    class CV
+    public class CV
     {
         private string? _specialty;
 
@@ -221,23 +256,225 @@
             get { return _languagesLevel; }
             set
             {
-                if (value!=null && (value.ContainsValue("A1")  || value.ContainsValue("A2") ||
+                if (value != null && (value.ContainsValue("A1") || value.ContainsValue("A2") ||
                    value.ContainsValue("B1") || value.ContainsValue("B2") ||
                    value.ContainsValue("C1") || value.ContainsValue("C2")))
                     _languagesLevel = value;
-                
+
             }
         }
 
         public bool HasDistinctionDiploma { get; set; }
 
 
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
+        public CV()
         {
 
+        }
+        public CV(string? specialty, string? schoolName, int score, List<string>? skills, List<string>? previousCompanies, DateTime startDate, DateTime previousJobEndDate, Dictionary<string, string>? langsLevel, bool hasDistinctionDiploma)
+        {
+            Specialty = specialty;
+            SchoolName = schoolName;
+            Score = score;
+            Skills = skills;
+            PreviousCompanies = previousCompanies;
+            StartDate = startDate;
+            PreviousJobEndDate = previousJobEndDate;
+            LangsLevel = langsLevel;
+            HasDistinctionDiploma = hasDistinctionDiploma;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void WriteJsonWorkers(List<Worker> workers)
+        {
+            var jsonSerializer = new JsonSerializer();
+            using (var sw = new StreamWriter("workers.json"))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    jsonSerializer.Serialize(jw, workers);
+                }
+            }
+        }
+
+        static List<Worker> ReadJsonWorkers()
+        {
+            var serializer = new JsonSerializer();
+            using (var sr = new StreamReader("workers.json"))
+            {
+                using (var jr = new JsonTextReader(sr))
+                {
+                    var workers = serializer.Deserialize<List<Worker>>(jr);
+                    return workers ?? [];
+                }
+            }
+        }
+
+        static void WriteXMLEmployers(List<Employer> employers)
+        {
+            var xml = new XmlSerializer(typeof(List<Employer>));
+            using (var fs = new FileStream("employers.xml", FileMode.OpenOrCreate))
+            {
+                xml.Serialize(fs, employers);
+            }
+        }
+
+        static List<Employer> ReadXMLEmployers()
+        {
+            List<Employer>? employers = null;
+            var xml = new XmlSerializer(typeof(List<Employer>));
+            using (var fs = new FileStream("employers.xml", FileMode.OpenOrCreate))
+            {
+                employers = xml.Deserialize(fs) as List<Employer>;
+            }
+            return employers ?? [];
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<Worker> workers = new List<Worker>{
+                new Worker(
+                    "Əli",
+                    "Məmmədov",
+                    "Bakı",
+                    "+994501234567",
+                    28,
+                    new CV(
+                        "Software Developer",
+                        "Bakı Dövlət Universiteti",
+                        450,
+                        new List<string> { "C#", "JavaScript", "SQL", "React" },
+                        new List<string> { "PASHA Bank", "Kapital Bank" },
+                        new DateTime(2018, 6, 15),
+                        new DateTime(2024, 12, 31),
+                        new Dictionary<string, string> { { "English", "C1" }, { "Russian", "B2" }, { "Turkish", "A2" } },
+                        true
+                    )
+                ),
+                new Worker(
+                    "Ayşə",
+                    "Həsənova",
+                    "Gəncə",
+                    "+994552345678",
+                    34,
+                    new CV(
+                        "Accountant",
+                        "Azərbaycan Dövlət İqtisad Universiteti",
+                        520,
+                        new List<string> { "Excel", "1C", "Financial Analysis", "Tax Planning" },
+                        new List<string> { "Azərsun Holding", "Azerenerji" },
+                        new DateTime(2015, 9, 1),
+                        new DateTime(2024, 11, 30),
+                        new Dictionary<string, string> { { "English", "B2" }, { "Russian", "C1" } },
+                        false
+                    )
+                ),
+                new Worker(
+                    "Rəşad",
+                    "Quliyev",
+                    "Sumqayıt",
+                    "+994503456789",
+                    42,
+                    new CV(
+                        "Mechanical Engineer",
+                        "Azərbaycan Texniki Universiteti",
+                        380,
+                        new List<string> { "AutoCAD", "SolidWorks", "Project Management" },
+                        new List<string> { "SOCAR", "Azərikimya" },
+                        new DateTime(2010, 3, 10),
+                        new DateTime(2024, 10, 15),
+                        new Dictionary<string, string> { { "English", "B1" }, { "Russian", "C2" } },
+                        true
+                    )
+                ),
+                new Worker(
+                    "Leyla",
+                    "Əliyeva",
+                    "Bakı",
+                    "+994554567890",
+                    29,
+                    new CV(
+                        "Marketing Specialist",
+                        "Azərbaycan Dövlət İqtisad Universiteti",
+                        495,
+                        new List<string> { "Digital Marketing", "SEO", "Content Creation", "Google Analytics" },
+                        new List<string> { "Bakcell", "Azercell" },
+                        new DateTime(2019, 2, 20),
+                        new DateTime(2025, 1, 10),
+                        new Dictionary<string, string> { { "English", "C2" }, { "Turkish", "B1" } },
+                        true
+                    )
+                ),
+                new Worker(
+                    "Elvin",
+                    "İbrahimov",
+                    "Mingəçevir",
+                    "+994505678901",
+                    38,
+                    new CV(
+                        "Civil Engineer",
+                        "Azərbaycan Memarlıq və İnşaat Universiteti",
+                        410,
+                        new List<string> { "Construction Management", "Structural Design", "AutoCAD" },
+                        new List<string> { "Akkord", "AzInşaat" },
+                        new DateTime(2012, 7, 5),
+                        new DateTime(2024, 9, 1),
+                        new Dictionary<string, string> { { "English", "B2" }, { "Russian", "B2" } },
+                        false
+                    )
+                )
+            };
+
+            WriteJsonWorkers(workers);
+            List<Employer> employers = new List<Employer>
+            {
+                new Employer(
+                    "Günay",
+                    "Mustafayeva",
+                    "Bakı",
+                    "+994556789012",
+                    45,
+                    new List<string> { "Software Developer", "DevOps Engineer", "QA Tester" }
+                ),
+                new Employer(
+                    "Tural",
+                    "Rzayev",
+                    "Bakı",
+                    "+994507890123",
+                    52,
+                    new List<string> { "Accountant", "Financial Analyst", "Auditor" }
+                ),
+                new Employer(
+                    "Sevil",
+                    "Abdullayeva",
+                    "Gəncə",
+                    "+994558901234",
+                    39,
+                    new List<string> { "Marketing Manager", "Social Media Specialist", "Brand Manager" }
+                ),
+                new Employer(
+                    "Kamran",
+                    "Hüseynov",
+                    "Sumqayıt",
+                    "+994509012345",
+                    48,
+                    new List<string> { "Mechanical Engineer", "Production Manager", "Quality Control Specialist" }
+                ),
+                new Employer(
+                    "Nərgiz",
+                    "Məhərrəmova",
+                    "Bakı",
+                    "+994550123456",
+                    41,
+                    new List<string> { "Civil Engineer", "Project Manager", "Site Supervisor" }
+                )
+            };
+
+            WriteXMLEmployers(employers);
         }
     }
 }
